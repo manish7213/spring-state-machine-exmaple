7daad48161e8123e5a67a3ecdd29342916f4da8e
manish7213 <manish.1si11cs057@gmail.com>
2020-10-02 18:02:09 +0530

    Initial Commit

---
+++ src/main/java/com/example/springstatemachineexmaple/SpringStateMachineExmapleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springstatemachineexmaple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStateMachineExmapleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStateMachineExmapleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springstatemachineexmaple/SpringStateMachineExmapleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springstatemachineexmaple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStateMachineExmapleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
